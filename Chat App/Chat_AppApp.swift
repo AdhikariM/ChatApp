e81d08ab94f6563f78475223b9556f831edcc7a6
AdhikariM <maheshadhikari@live.com>
2024-02-11 22:31:25 -0600

    Initial Commit

---
+++ Chat App/Chat_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chat_AppApp.swift
+//  Chat App
+//
+//  Created by Mahesh Adhikari on 2/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Chat_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
